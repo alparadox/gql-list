3332d1b95abcf558f7abba45af556ed2a14dc9c1
Alyona Duhnova <alyona.dux@yandex.ru>
2022-08-14 19:41:10 +0300

    добавлена радиокнопка

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 
 
 @NgModule({
   declarations: [
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    CheckboxComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    InputComponent,
+    RadioComponent,
+    CheckboxComponent
   ]
 })
 export class SharedModule { }
